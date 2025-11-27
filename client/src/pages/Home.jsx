ac9108d62ec0d6333cc2eb10b4d261b63f01198e
Swayam Shinde <shindeswayam2004@gmail.com>
2025-11-27 23:22:05 +0530

    added pages

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
-import Hero from '../components/Hero';
-import AiTools from '../components/AiTools';
-import Testimonial from '../components/Testimonial';
-import Plan from '../components/Plan';
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import AiTools from '../components/AiTools'
+import Testimonial from '../components/Testimonial'
+import Plan from '../components/Plan'
+import Footer from '../components/Footer'
 
 const Home = () => {
-    return (
-      <>
+  return (
+    <>
       <Navbar />
       <Hero />
-      <AiTools />
+      <AiTools/>
       <Testimonial />
-      <Plan />
-      </>
-    );
+      <Plan/>
+      <Footer/>
+    </>
+  )
 }
 
-export default Home;
+export default Home
